19a13e07c6bd81cf42caf9b39c9bba511f729061
Andrinha <andrinha.eru@gmail.com>
2022-06-04 18:19:14 +0400

    initial commit

---
+++ app/src/main/java/com/fit/bullsandcows/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fit.bullsandcows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fit.bullsandcows.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/fit/bullsandcows/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.fit.bullsandcows.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
